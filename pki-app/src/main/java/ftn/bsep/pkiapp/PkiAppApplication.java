60455810e9e4bf4213c7a812a37aeea1ef538177
lakovicj <31940189+lakovicj@users.noreply.github.com>
2020-04-04 17:06:03 +0200

    init

---
+++ pki-app/src/main/java/ftn/bsep/pkiapp/PkiAppApplication.java
@@ -0,0 +1,13 @@
+package ftn.bsep.pkiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkiAppApplication.class, args);
+	}
+
+}
---
+++ pki-app/src/test/java/ftn/bsep/pkiapp/PkiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ftn.bsep.pkiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
